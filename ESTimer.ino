809ee4e80b1e61b5fea26c46018bcfa0e507e4fd
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-01-19 16:23:31 +0330

    Release v2.0.0

--- ESTimer.ino
+++ ESTimer.ino
@@ -1,169 +1,254 @@
-#define INCLUDE_OLED          false    // Change it to true if you assemble monitor version
-#define BRIGHTNESS_OLED       0.5      // In Range of 0 to 1
-#define FLIP_CONTENT          false    // If you have connect your ES Timer to right side of computer should change it to true 
+#define WINDOWS               0x01
+#define LINUX                 0x02
+#define MACOS                 0x03
 
-#define BE_SAVE_TIMER_STATUS  true     // If you don't want to save status of the timer (awake and sleep), change it to false
+#define SYSTEM_OS             WINDOWS                  // To go to the Lock screen, you must specify your operating system type (WINDOWS or LINUX or MACOS).
 
-#include <ESTimer.h>
-#if INCLUDE_OLED
-#include "Fonts.h"
-#include "Images.h"
-#endif
+#define INCLUDE_OLED          false                    // Change it to true if you assemble monitor version.
+#define ADDRESS_OLED          0x3C                     // Usually the ssd1306 address is this value, but if your ssd1306 value was different you can change it (use Digispark_Examples/i2cScanner to get it).
+#define BRIGHTNESS_OLED       0.5                      // In the range 0 to 1, where 1 is the maximum OLED brightness and 0 means it is completely off.
+#define FLIP_CONTENT_OLED     false                    // If you have connect your ES Timer to right side of computer should change it to true.
+
+#define WORK_DURATION         minutes(25) + seconds(0) // The duration of each work can be a maximum of 99 minutes, which you can use up to a second accuracy.
+#define SHORT_BREAK_DURATION  minutes(5) + seconds(0)  // The duration of each short break can be a maximum of one-fifth work time, which you can use up to a second accuracy.
+#define LONG_BREAK_RATE       4.0                      // There should be take a long break after all four pomodoros, which is four times the short break by default.
+#define PAUSABLE_WORK_PHASE   false                    // If you want the work phase counter to pause when the timer is unplugged and resume it when again plugged, change it to true.
+
+#define _WORK_DURATION min(WORK_DURATION, minutes(99))
+#define BREAK_DURATION min((min(SHORT_BREAK_DURATION, WORK_DURATION / 5) * (currentTimerPhase == SHORT_BREAK ? 1 : max(LONG_BREAK_RATE, 1))), _WORK_DURATION)
 
 #define seconds(n) (n)
 #define minutes(n) (n * seconds(60))
 
-#define AWAKE_TIME min(minutes(25), minutes(99))
-#define SLEEP_TIME min(minutes(5), AWAKE_TIME / 4)
+#include "Images.h"
+#include "Fonts.h"
 
-#define LED_PIN 1
-#define TIMER_STATUS_ADDRESS 0
-#define COUNT_DP_ADDRESS     TIMER_STATUS_ADDRESS + sizeof(timerStatus)
+#include <ESTimer.h>
+#include <EEPROM.h>
 
-bool timerStatus = true;
-uint8_t countDonePomodoros = 0;
+#define REMAINING_CD_ADDRESS  0
+#define COUNT_POMODO_ADDRESS  OFFSET_RCD_ADDRESS - sizeof(countPomodoros)
+#define OFFSET_RCD_ADDRESS    WORK_TIME_ADDRESS - sizeof(offsetRCD)
+#define WORK_TIME_ADDRESS     MAX_ADDRESS - sizeof(workTime)
+#define MAX_ADDRESS           512
 
-bool isCompletedRest = false;
+enum TimerPhase { WORK, SHORT_BREAK, LONG_BREAK };
+
+TimerPhase currentTimerPhase = WORK;
+uint16_t workTime = _WORK_DURATION;
+uint16_t offsetRCD = 0;
+uint8_t countPomodoros = 0;
+uint16_t remainingCountdownTime = 0;
 
 void setup() {
+  setPinsMode();
   initTimer();
-  startTimer();
+#if INCLUDE_OLED
+  initOLED();
+#endif
 }
 
 void loop() {
-  while (ESTimer.onSleepMode()) {
-    // This part of the code runs only when Windows is on sleep mode
-    if (!isCompletedRest) {
 #if INCLUDE_OLED
-      updateStatusPomodoros();
+  showCurrentTimerPomodoro();
 #endif
-      startCountdownTimer();
 
-      EEPROM.put(COUNT_DP_ADDRESS, ++countDonePomodoros);
-      isCompletedRest = true;
-    }
-    flashLight();
-  }
+  startTimerWorkPhase();
+  goToSystemLockScreen();
+  startTimerBreakPhase();
+  
+  increaseCountPomodoros();
+  awaitToNextPomodoroRequest();
 
-  // This part of the code runs only when Windows is ready
-  if (isCompletedRest) {
-    startTimer();
-  }
+#if INCLUDE_OLED
+  updateCompletedTimerPomodoros();
+#endif
+}
+
+void setPinsMode() {
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void initTimer() {
-  DDRB |= 0b10; // pinMode(LED_PIN, OUTPUT);
-
-  EEPROM.get(COUNT_DP_ADDRESS, countDonePomodoros);
-
-#if INCLUDE_OLED
-#if FLIP_CONTENT
-  ESTimer.flipContent();
-#endif
-  splash();
-
-  initNumbers();
-  initStatusPomodoros();
-#endif
+  changeImmediatelyWorkTimeIfWasChanged();
+  fetchValuesFromEEPROM();
 }
 
-void startTimer() {
-  PORTB &= 0b01; // digitalWrite(LED_PIN, LOW);
-
-#if BE_SAVE_TIMER_STATUS
-  EEPROM.get(TIMER_STATUS_ADDRESS, timerStatus);
+void changeImmediatelyWorkTimeIfWasChanged() {
+  uint16_t temp;
+  EEPROM.get(WORK_TIME_ADDRESS, temp);
+  if (temp != workTime) {
+#if PAUSABLE_WORK_PHASE
+    safeStoreRemainingTime(0);
 #endif
-
-  if (timerStatus) {
-#if INCLUDE_OLED
-    updateStatusPomodoros();
-#endif
-
-    if (!ESTimer.onSleepMode()) {
-      startCountdownTimer();
-    } else {
-      while (ESTimer.onSleepMode()) flashLight();
-      startTimer();
-    }
+    EEPROM.put(WORK_TIME_ADDRESS, workTime);
   }
-
-  while (!ESTimer.onSleepMode()) {
-    ESTimer.goToSleepMode();
-    ESTimer.delay(100);
-  }
-  isCompletedRest = false;
 }
 
-void flashLight() {
-  PORTB |= 0b10; // digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  PORTB &= 0b01; // digitalWrite(LED_PIN, LOW);
+void fetchValuesFromEEPROM() {
+#if PAUSABLE_WORK_PHASE
+  EEPROM.get(OFFSET_RCD_ADDRESS, offsetRCD);
+  EEPROM.get(REMAINING_CD_ADDRESS + offsetRCD, remainingCountdownTime);
+#endif
+  EEPROM.get(COUNT_POMODO_ADDRESS, countPomodoros);
+}
+
+void startTimerWorkPhase() {
+  if (timerPhaseIsWork()) startCountdownTimer();
+}
+
+void goToSystemLockScreen() {
+  ESTimer.goToSystemLockScreen();
+  ESTimer.delay(500);
+}
+
+void startTimerBreakPhase() {
+  if (!timerPhaseIsWork()) startCountdownTimer();
+}
+
+void increaseCountPomodoros() {
+  EEPROM.put(COUNT_POMODO_ADDRESS, ++countPomodoros);
+}
+
+void awaitToNextPomodoroRequest() {
+  while (!ESTimer.receivedRequestFromUser()) turnOnOffLEDWithDelay();
+}
+
+void turnOnOffLEDWithDelay() {
+  turnOnLED();
+  ESTimer.delay(500);
+  turnOffLED();
+  ESTimer.delay(500);
+}
+
+void turnOnLED() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void turnOffLED() {
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void startCountdownTimer() {
-  uint16_t totalTime = timerStatus ? AWAKE_TIME : SLEEP_TIME * ((countDonePomodoros + 1) % 4 == 0 ? 4 : 1);
+  uint16_t remainingTime = timerPhaseIsWork() ? (remainingCountdownTime > 0 ? remainingCountdownTime : _WORK_DURATION) : BREAK_DURATION;
 
   do {
-    uint16_t previousTime = (uint16_t) millis();
+    uint16_t start = (uint16_t) millis();
 
 #if INCLUDE_OLED
-    uint8_t m0 = (totalTime / 60) / 10;
-    uint8_t m1 = (totalTime / 60) % 10;
-    uint8_t s0 = (totalTime % 60) / 10;
-    uint8_t s1 = (totalTime % 60) % 10;
-
-    drawNumbers(m0, m1, s0, s1);
+    updateTimerCounter(remainingTime);
 #endif
 
-    if (!timerStatus) ESTimer.goToSleepMode();
-
-    ESTimer.delay(1000 - ((uint16_t) millis() - previousTime));
-  } while (totalTime-- > 0);
-
-  timerStatus = !timerStatus;
-#if BE_SAVE_TIMER_STATUS
-  EEPROM.put(TIMER_STATUS_ADDRESS, timerStatus);
+    if (timerPhaseIsWork()) {
+#if PAUSABLE_WORK_PHASE
+      safeStoreRemainingTime(remainingTime);
 #endif
+      if (remainingTime <= seconds(15)) turnOnLED();      
+    } else {
+      ESTimer.goToSystemLockScreen();
+      turnOffLED();
+    }
+    
+    uint16_t takenTime = (uint16_t) millis() - start;
+    ESTimer.delay(1000 - takenTime);
+  } while (remainingTime-- > 0);
+
+  changeTimerPhaseToNext();
 }
 
-#if INCLUDE_OLED
-void splash() {
-  ESTimer.drawBitmap(46, 0, 82, 8, es_logo);
-  ESTimer.delay(3000);
-  ESTimer.clear();
+void safeStoreRemainingTime(uint16_t remainingTime) {
+  uint16_t temp;
+  do {
+    EEPROM.put(REMAINING_CD_ADDRESS + offsetRCD, remainingTime);
+    EEPROM.get(REMAINING_CD_ADDRESS + offsetRCD, temp);
+  } while (temp != remainingTime && increaseOffsetRCD());
+  EEPROM.get(REMAINING_CD_ADDRESS + offsetRCD, remainingCountdownTime);
 }
 
-void drawNumbers(uint8_t m0, uint8_t m1, uint8_t s0, uint8_t s1) {
-  ESTimer.drawBitmap(65, 1, 100, 4, numbers[m0]);
-  ESTimer.drawBitmap(65, 4, 100, 7, numbers[m1]);
-  ESTimer.drawBitmap(28, 1, 63,  4, numbers[s0]);
-  ESTimer.drawBitmap(28, 4, 63,  7, numbers[s1]);
+bool increaseOffsetRCD() {
+  offsetRCD += sizeof(remainingCountdownTime);
+  EEPROM.put(OFFSET_RCD_ADDRESS, offsetRCD);
+  return true;
 }
 
-void initNumbers() {
-  uint8_t t = 0;
-  drawNumbers(t, t, t, t);
+bool timerPhaseIsWork() {
+  return currentTimerPhase == WORK;
 }
 
-void initStatusPomodoros() {
-  for (uint8_t i = 0; i < 8; i++) {
-    drawPomodoro(i, dots[(i <= countDonePomodoros % 8) ^ ((countDonePomodoros / 8) % 2)]);
+void changeTimerPhaseToNext() {
+  currentTimerPhase = getNextTimerPhase();
+}
+
+TimerPhase getNextTimerPhase() {
+  switch (currentTimerPhase) {
+    case SHORT_BREAK:
+    case LONG_BREAK:
+      return WORK;
+    default:
+      uint8_t nextPomodoro = countPomodoros + 1;
+      return nextPomodoro % 4 ? SHORT_BREAK : LONG_BREAK;
   }
 }
 
-void updateStatusPomodoros() {
-  uint8_t i = countDonePomodoros % 8;
-  drawPomodoro(i, dots[((countDonePomodoros / 8) + 1) % 2]);
+void initOLED() {
+  ESTimer.initSSD1306();
+  
+  showSplashScreen();
+  showTimerCounter();
+  showCompletedTimerPomodoros();
 }
 
-void drawPomodoro(uint8_t index, const byte *s) {
+void showSplashScreen() {
+  ESTimer.drawBitmap(46, 0, 82, 8, es_logo);
+  ESTimer.delay(3000);
+  ESTimer.clearOLED();
+}
+
+void showTimerCounter() {
+  uint8_t zero = 0;
+  drawNumbers(zero, zero, zero, zero);
+}
+
+void showCompletedTimerPomodoros() {
+  for (uint8_t index = 0; index < 8; index++) {
+    drawPomodoro(index, dots[(index < countPomodoros % 8) ^ ((countPomodoros / 8) % 2)]);
+  }
+}
+
+void updateTimerCounter(uint16_t totalTime) {
+  uint8_t leftDigitOfMinute = (totalTime / 60) / 10;
+  uint8_t rightDigitOfMinute = (totalTime / 60) % 10;
+  uint8_t leftDigitOfSecound = (totalTime % 60) / 10;
+  uint8_t rightDigitOfSecound = (totalTime % 60) % 10;
+
+  drawNumbers(leftDigitOfMinute, rightDigitOfMinute, leftDigitOfSecound, rightDigitOfSecound);
+}
+
+void updateCompletedTimerPomodoros() {
+  uint8_t countCompletedPomodoros = countPomodoros - 1;
+  uint8_t index = countCompletedPomodoros % 8;
+  drawPomodoro(index, dots[((countCompletedPomodoros / 8) + 1) % 2]);
+}
+
+void showCurrentTimerPomodoro() {
+  uint8_t index = countPomodoros % 8;
+  drawPomodoro(index, dots[2 + (countPomodoros / 8) % 2]);
+}
+
+void drawNumbers(uint8_t topLeft, uint8_t topRight, uint8_t bottomLeft, uint8_t bottomRight) {
+  ESTimer.drawBitmap(65, 1, 100, 4, numbers[topLeft]);
+  ESTimer.drawBitmap(65, 4, 100, 7, numbers[topRight]);
+  ESTimer.drawBitmap(28, 1, 63,  4, numbers[bottomLeft]);
+  ESTimer.drawBitmap(28, 4, 63,  7, numbers[bottomRight]);
+}
+
+void drawPomodoro(uint8_t index, const byte *bitmap) {
   uint8_t rowOffset = index < 4 ? 104 : 0;
   ESTimer.drawBitmap(
     4 + rowOffset,
     2 * (index % 4),
     20 + rowOffset,
     (2 * (index % 4)) + 2,
-    s
+    bitmap
   );
 }
-#endif
